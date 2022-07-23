878b373c9bb2429ae0947b88a5bf58f68b200d48
unknown <ziadtarek795@gmail.com>
2022-07-23 15:50:59 +0200

    Modified Modules

--- Timer.v
+++ Timer.v
@@ -2,8 +2,11 @@ module Timer (
 input  wire  Rst,
 input  wire  Clk,
 input  wire [1:0] Clk_Freq,
-output reg time_out,
-output reg time_out_s
+input  wire [2:0] Timer_Encoding,
+input  wire Pause_Enable_T,
+output reg  Time_Event
+//output reg  time_out_s ,
+//output reg  time_out
 );
 
 reg [15:0] Counter;
@@ -11,12 +14,13 @@ reg [15:0] Cycle_Counter;
 reg [15:0] S_Counter;
 reg [15:0] M_Counter;
 
+// --------------Different Frequency---------------
 localparam Clk_8MHz = 16'd122;
 localparam Clk_4MHz = 16'd61;
 localparam Clk_2MHz = 16'd31;
 localparam Clk_1MHz = 16'd15;
 
-reg [15:0] D;
+reg  [15:0] D;
 reg  [15:0] Q;
 
 always @ (*)
@@ -37,17 +41,58 @@ begin
         Q <= D;
 end
 
+// --------------Different Timming---------------
+localparam Min_1 = 16'd0;
+localparam Min_2 = 16'd1;
+localparam Min_5 = 16'd4;
+
+reg  [15:0] D_2;
+reg  [15:0] Q_2;
+
+always @ (*)
+begin
+    case (Timer_Encoding) 
+        3'b001:  D_2 = Min_2;
+        3'b010:  D_2 = Min_5;
+        3'b011:  D_2 = Min_2;
+        3'b100:  D_2 = Min_1;
+        default: D_2 = Min_1;
+    endcase
+end
+
 always @ (posedge Clk or negedge Rst)
 begin
     if (!Rst)
+        Q_2 <= 16'd0;
+    else 
+        Q_2 <= D_2;
+end
+
+
+// --------------logic---------------
+
+always @ (posedge Clk or negedge Rst)
+begin
+
+    if (!Rst)
         begin
-            Counter <= 16'h0000;
+            Counter       <= 16'h0000;
             Cycle_Counter <= 16'h0000;
-            S_Counter <= 16'h0000;
-            M_Counter <= 16'h0000;
-            time_out <= 1'b0;
-            time_out_s <= 1'b0;
+            S_Counter     <= 16'h0000;
+            M_Counter     <= 16'h0000;
+            Time_Event    <= 1'b0;
+            //time_out      <= 1'b0;
+            //time_out_s    <= 1'b0;
         end
+
+    else if (Pause_Enable_T ==1)
+        begin
+            Counter       <=  Counter;
+            Cycle_Counter <=  Cycle_Counter;
+            S_Counter     <=  S_Counter;
+            M_Counter     <=  M_Counter;
+        end
+
     else 
         begin
             if (Counter == 16'hFFFF)
@@ -56,20 +101,40 @@ begin
                     if (Cycle_Counter == Q )
                         begin
                             Cycle_Counter <= 16'h0000;
-                            time_out <= 1'b1;  //1  second
+                            //time_out <= 1'b1;  //1  second
                             if (S_Counter == 16'd1)
                                 begin
                                     S_Counter <=16'h0000;
-                                    time_out_s <=1'b1;            // --> 16'd1 = 2 sec  --> 16'd59 = 1 min
+                                    //time_out_s <=1'b1;            // --> 16'd1 = 2 sec  --> 16'd59 = 1 min
+                                    //if (M_Counter == Q_2)
+                                    if (M_Counter == 16'd0)
+                                        begin
+                                            M_Counter  <= 16'h0000;
+                                            Time_Event <= 1'b1;    // --> 16'd0 = 1 min  
+                                        end
+                                    else
+                                        begin
+                                            Time_Event    <= 1'b0;
+                                            M_Counter <= M_Counter + 1'b1;
+                                        end
                                 end 
                             else 
-                                S_Counter <= S_Counter + 1'b1;
+                                begin
+                                    Time_Event    <= 1'b0;
+                                    S_Counter <= S_Counter + 1'b1;
+                                end
                         end
                     else
-                        Cycle_Counter <= Cycle_Counter + 1'b1;
+                        begin
+                            Time_Event    <= 1'b0; 
+                            Cycle_Counter <= Cycle_Counter + 1'b1;
+                        end
                 end
             else 
-                Counter <= Counter + 1'b1;
+                begin
+                    Time_Event    <= 1'b0;
+                    Counter <= Counter + 1'b1;
+                end
         end
 end
 
--- Timer_tb.v
+++ Timer_tb.v
@@ -6,9 +6,11 @@ module Timer_tb ();
 reg  Rst_TB;
 reg  Clk_TB;
 reg  [1:0] Clk_Freq_TB;
-wire time_out_TB;
-wire time_out_s_TB;
-
+reg  Pause_Enable_T_TB;
+reg  [2:0] Timer_Encoding_TB;
+wire Time_Event_TB;
+//wire time_out_s_TB;
+//wire time_out_TB;
 /////////////// Clk Generator ///////////////
 localparam Period = 125;
 always #(Period/2) Clk_TB = ~Clk_TB;     // Clk Frequency 8 MHz
@@ -22,10 +24,17 @@ initial
         Initialization ();
         Reset ();
         $display ("the initial time %t",$time);
-        wait (time_out_TB == 1);
-        $display ("the S time %t",$time);  // 1 second
+        /*wait (time_out_TB == 1);
+        $display ("the S time %t",$time);  // 1 minute   ----> 2 sec
         wait (time_out_s_TB == 1);
-        $display ("the M time %t",$time);  // 1 minute
+        $display ("the S time %t",$time);  // 1 minute   ----> 2 sec*/
+        wait (Time_Event_TB == 1);
+        $display ("the M time %t",$time);  // 1 minute   ----> 2 sec
+        #2000
+        Trial (1'b1);
+        #2000
+        Trial (1'b0);
+        #2000
         $stop;
     end
 
@@ -45,6 +54,15 @@ task Initialization;
     begin
         Clk_TB=1'b1;
         Clk_Freq_TB=2'b11;
+        Pause_Enable_T_TB = 1'b0;
+        Timer_Encoding_TB=3'b000;
+    end
+endtask
+
+/////////////// Trial BLock ///////////////
+task Trial (input value);
+    begin
+        Pause_Enable_T_TB = value;
     end
 endtask
 
@@ -53,8 +71,11 @@ Timer U1 (
 .Rst(Rst_TB),
 .Clk(Clk_TB),
 .Clk_Freq(Clk_Freq_TB),
-.time_out(time_out_TB),
-.time_out_s(time_out_s_TB)
+.Pause_Enable_T(Pause_Enable_T_TB),
+.Timer_Encoding(Timer_Encoding_TB),
+.Time_Event(Time_Event_TB)
+//.time_out_s (time_out_s_TB),
+//.time_out(time_out_TB)
 );
 
 endmodule
